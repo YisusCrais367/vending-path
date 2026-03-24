13de2280436e72a251cb8827ba215508a98f84db
Miker888Gamer <miker888torres@gmail.com>
2026-03-24 10:57:26 -0700

    este es el mensajedel comimit

---
+++ entidades/ventas.py
@@ -0,0 +1,7 @@
+class Producto:
+
+def __init__(self,nombre: str ,precio: float ,cantidad: int ,tipo: TipoProducto ,):
+    self.nombre = nombre
+    self.precio = precio
+    self.cantidad = cantidad
+    self.tipo = tipo
