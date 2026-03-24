f3739078cb21087cbb4816ab0b4369810797fa37
YisusCrais367 <jesusricardoleonruiz@gmail.com>
2026-03-24 11:20:37 -0700

    agrege el main

--- entidades/ventas.py
+++ entidades/ventas.py
@@ -1,7 +1,8 @@
+from entidades.producto import TipoProducto
 class Producto:
 
-def __init__(self,nombre: str ,precio: float ,cantidad: int ,tipo: TipoProducto ,):
-    self.nombre = nombre
-    self.precio = precio
-    self.cantidad = cantidad
-    self.tipo = tipo
+    def __init__(self,nombre: str,precio: float,cantidad: int,tipo: TipoProducto):
+        self.nombre = nombre
+        self.precio = precio
+        self.cantidad = cantidad
+        self.tipo = tipo
---
+++ main.py
@@ -0,0 +1,12 @@
+from entidades.ventas import Producto
+
+print("Bienvenido a la maquina expendedora de productos de la empresa ICKKCK con productos de la mas altisima calidad, que legalmente estan registrados en la oficina de patentes y marcas de los Estados Unidos, con el numero de patente 12345678910, y que cumplen con todos los estandares de calidad y de seguridad establecidos por la FDA, y que ademas son 100% veganos, organicos y libres de gluten, y que ademas son productos que han sido probados por un equipo de expertos especializados en la industria de los productos de cunsumo, y que ademas son productos que han sido recomendados por los mejores nutricionistas del mundo, y que ademas son productos que han sido aprobados por la comunidad internacional de consumidores, y que ademas son productos que han sido galardonados con premios internacionales por su calidad y sabor excepcionales")
+print("""Por favor seleccione el producto que desea comprar:
+      1. Pan
+      2. Bebida
+      3. Snack
+      4. Otro
+      5. Salir""")
+
+eleccion_c = int(input("----->"))
+
